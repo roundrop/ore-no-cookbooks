6a76a524f9080234a4190d6f63c91fb1d70cb4c1
roundrop <roundrop@gmail.com>
2014-01-21 14:29:59 +0900

    php 5.5.8

--- site-cookbooks/php/attributes/default.rb
+++ site-cookbooks/php/attributes/default.rb
@@ -1,2 +1,2 @@
-default['php']['version'] = '5.5.7'
+default['php']['version'] = '5.5.8'
 default['php']['packages'] = %w{libcurl-devel re2c libmcrypt-devel libxml2-devel bison bison-devel libjpeg-devel libpng-devel readline-devel libtidy-devel libxslt-devel enchant-devel libXpm-devel freetype-devel t1lib-devel gmp-devel libc-client-devel libicu-devel oniguruma-devel net-snmp net-snmp-devel bzip2-devel}
